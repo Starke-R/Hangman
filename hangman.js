77cbbb4c13b5ae04c0e66183c0569c670ad13798
starkeOnline <starke.robin@outlook.com>
2023-11-13 16:30:13 +0100

    add

---
+++ hangman.js
@@ -0,0 +1,18 @@
+fetch("https://random-word-api.herokuapp.com/word?lang=en")
+   .then(response => response.json())
+   .then((json) => {
+
+    let word = json[0];
+    let wordNum = "";
+    let mainDiv = document.createElement("div");
+    for (let i = 0; i < word.length; i++) {
+        wordNum = wordNum + "_ ";
+    }
+    
+    mainDiv.innerHTML = wordNum;
+
+    document.getElementById("mainContainer").append(mainDiv);
+
+   }
+
+   )
