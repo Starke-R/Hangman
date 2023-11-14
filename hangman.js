8e4369c2cb51063c39acc59df2db5a7730cef776
starkeOnline <starke.robin@outlook.com>
2023-11-14 17:08:12 +0100

    Alert if more than one letter in entered in the guessing field

--- hangman.js
+++ hangman.js
@@ -1,17 +1,68 @@
 fetch("https://random-word-api.herokuapp.com/word?lang=en")
-   .then(response => response.json())
-   .then((json) => {
+    .then(response => response.json())
+    .then((json) => {
 
-    let word = json[0];
-    let wordNum = "";
-    let mainDiv = document.createElement("div");
-    for (let i = 0; i < word.length; i++) {
-        wordNum = wordNum + "_ ";
+        let word = json[0];
+        let wordNum = "";
+        let wordLetter = "";
+
+        let letterDiv = document.createElement("div");
+        
+        for (let i = 0; i < word.length; i++) {
+            wordLetter = word;
+        }
+        letterDiv.innerText = wordLetter;
+        letterDiv.classList.add("randomWords");
+
+        document.getElementById("mainContainer").append(letterDiv);
+
+
+
+
+        let underscoreDiv = document.createElement("div");
+
+        for (let i = 0; i < word.length; i++) {
+            wordNum = wordNum + "_ ";
+        }
+
+        
+        underscoreDiv.innerText = wordNum;
+        underscoreDiv.classList.add("randomWords");
+
+        document.getElementById("mainContainer").append(underscoreDiv);
+
+
+
+
+
+
+        let input = document.getElementById("guessLetter");
+        input.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                let guess = document.getElementById("guessLetter").value;
+
+                if (guess.length > 1 ){
+                    alert("No more than one letter at a time!");
+                }
+
+                else {
+                
+                for (let i = 0; i < word.length; i++) {
+                   if (guess == word[i]){
+                    console.log(guess)
+                   }
+                   
+                }
+        
+                }
+        
+            }
+        });
+
+
     }
-    
-    mainDiv.innerHTML = wordNum;
+    )
 
-    document.getElementById("mainContainer").append(mainDiv);
-   }
 
-   )
+
+
